6444438e40fd7c100fa3c16ed024dd49adb5ec8c
Vu_Hoai_Lien <vulien4112004@gmail.com>
2025-07-30 14:30:32 +0700

    30-7-2025

---
+++ Buoi_8(30_7)/data.js
@@ -0,0 +1,38 @@
+// data.js
+const giaoDichData = [
+  {
+    id: 1102,
+    khachHang: "Vũ Hoài Liên",
+    nhanVien: "Mai Thục Anh",
+    soTien: 250000,
+    ngayMua: "06 Tháng 6 2024 9:00"
+  },
+  {
+    id: 1199,
+    khachHang: "Hoàng Thị Thắng",
+    nhanVien: "Nguyễn Văn Hồng",
+    soTien: 600000,
+    ngayMua: "06 Tháng 6 2024 9:02"
+  },
+  {
+    id: 1239,
+    khachHang: "Nguyễn Hữu Quang",
+    nhanVien: "Nguyễn Văn Hồng",
+    soTien: 934000,
+    ngayMua: "06 Tháng 6 2024 9:10"
+  },
+  {
+    id: 1277,
+    khachHang: "Nguyễn Văn Nam",
+    nhanVien: "Mai Thục Anh",
+    soTien: 930000,
+    ngayMua: "06 Tháng 6 2024 9:20"
+  },
+  {
+    id: 1439,
+    khachHang: "Nguyễn Hồng Minh",
+    nhanVien: "Mai Thục Anh",
+    soTien: 354000,
+    ngayMua: "06 Tháng 6 2024 9:24"
+  }
+];
